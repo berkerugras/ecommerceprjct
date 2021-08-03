9f70d11f9477213f8b34022ebb308bd7cf28a446
berkerugras <42471145+berkerugras@users.noreply.github.com>
2021-08-03 16:19:59 +0300

    First commit and Micro changes on routes.js file

---
+++ routes.js
@@ -0,0 +1,33 @@
+exports.index = function(req, res) {
+	res.render("index", {
+		// Template data
+		title: "Express"
+	});
+};
+
+
+exports.hello = function(req, res) {
+	var _         = require("underscore");
+	var mdbClient = require('mongodb').MongoClient;
+
+	mdbClient.connect("mongodb://localhost:27017/", function(err, client) {
+		if(err){
+			res.json(err)
+		}
+		let db=client.db("shop");
+		const collection = db.collection('categories');
+
+		collection.find().toArray(function(err, items) {
+			res.render("hello", {
+				// Underscore.js lib
+				_     : _,
+
+				// Template data
+				title : "Hello World!",
+				items : items
+			});
+
+			client.close();
+		});
+	});
+};
