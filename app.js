a468028f9585450112e07f95e5a5ebb3c2252004
berkerugras <42471145+berkerugras@users.noreply.github.com>
2021-08-04 19:42:29 +0300

    Express framework updated and the code modified accordingly

--- app.js
+++ app.js
@@ -4,24 +4,30 @@ var express = require("express")
   , path    = require("path")
   , routes  = require("./routes");
 var app     = express();
+var cookieParser = require('cookie-parser');
+var bodyParser=require('body-parser');
+var logger = require('morgan');
+var session=require('express-session');
+
+//routes
+var indexRouter = require('./routes/index');
+var helloRouter = require('./routes/hello');
 
 // All environments
 app.set("port", 80);
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
-app.use(express.favicon());
-app.use(express.logger("dev"));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(express.cookieParser("61d333a8-6325-4506-96e7-a180035cc26f"));
-app.use(express.session());
-app.use(app.router);
+app.use(logger("dev"));
+app.use(bodyParser.json());
+
+app.use(cookieParser("61d333a8-6325-4506-96e7-a180035cc26f"));
+app.use(session());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.errorHandler());
+// app.use(express.errorHandler());
 
 // App routes
-app.get("/"     , routes.index);
-app.get("/hello", routes.hello);
+app.use("/"     , indexRouter);
+app.use("/hello", helloRouter);
 
 // Run server
 http.createServer(app).listen(app.get("port"), function() {
